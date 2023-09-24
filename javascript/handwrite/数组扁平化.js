edb9c475d26df638f9847a08b9a19d09c54f582d
qishilong <663253062@qq.com>
2023-09-24 21:51:34 +0800

    实现数组扁平化
    实现数组扁平化

---
+++ javascript/handwrite/数组扁平化.js
@@ -0,0 +1,37 @@
+function myFlat(arr, num = 1) {
+	let result = [];
+	arr.forEach((item) => {
+		if (Array.isArray(item) && num > 0) {
+			result = result.concat(myFlat(item, num - 1));
+		} else {
+			result.push(item);
+		}
+	});
+	return result;
+}
+
+const arr = [
+	1,
+	2,
+	[1, 2, 3],
+	[12, 3],
+	[1, 2, 3],
+	1,
+	2,
+	34,
+	[1, 2],
+	3,
+	4,
+	{ a: 1 },
+	3,
+	[
+		12,
+		3,
+		1,
+		[2, [3, [1, 2, 3, [1, 2, [1, [2, [3, 2, 3, [12, [321, [32, 12, 3, [1, 2, 3]]]]]]]]]]],
+	],
+	{ a: 1 },
+];
+
+const result = myFlat(arr, Infinity);
+console.log(result);
