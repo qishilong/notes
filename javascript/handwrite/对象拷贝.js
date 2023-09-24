66ab1b830c06e470cf31cb6848727f83fe89e541
qishilong <663253062@qq.com>
2023-09-24 17:22:04 +0800

    手写深拷贝
    手写深拷贝

---
+++ javascript/handwrite/对象拷贝.js
@@ -0,0 +1,127 @@
+// const obj = {
+//     data: 1,
+//     un: undefined,
+//     info: {
+//         d: 2
+//     },
+//     fn: function () {
+//         console.log('Function');
+//     },
+//     get c() {
+//         return this.info.d;
+//     }
+// };
+
+/**
+ * 浅拷贝
+ * 1. Object.assign
+ * 2. 不完整的深拷贝 JSON.stringify & JSON.parse
+ * 3. Object.create
+ */
+// 1. Object.assign
+// const obj1 = Object.assign({}, obj);
+// const obj2 = Object.assign({}, obj);
+// console.log(obj1.info === obj2.info);   // true
+
+// 2. 不完整的深拷贝 JSON.stringify & JSON.parse
+// const obj1 = JSON.parse(JSON.stringify(obj));
+// const obj2 = JSON.parse(JSON.stringify(obj));
+// console.log(obj1, obj2);
+// console.log(obj1.info === obj2.info);   // false
+
+// 3. Object.create
+// const obj1 = Object.create(obj);
+// const obj2 = Object.create(obj);
+// console.log(obj1, obj2);
+// console.log(obj1.__proto__.info === obj2.__proto__.info);   // true
+
+/**
+ * 深拷贝
+ * 递归实现
+ * 这种写法无法解决循环引用的问题
+ */
+
+const checkType = (value) => {
+	return Object.prototype.toString.call(value).slice(8, -1);
+};
+function deepClone(obj) {
+	if (checkType(obj) === 'Object') {
+		const newObj = {};
+		for (const key in obj) {
+			newObj[key] = deepClone(obj[key]);
+		}
+		return newObj;
+	} else if (checkType(obj) === 'Array') {
+		const newArr = [];
+		const length = obj.length;
+		for (let i = 0; i < length; i++) {
+			newArr[i] = deepClone(obj[i]);
+		}
+		return newArr;
+	} else if (checkType(obj) === 'Function') {
+		return new Function(`return ${obj.toString()}`).call(this);
+	} else if (checkType(obj) === 'Date') {
+		return new Date(obj.valueOf());
+	} else if (checkType(obj) === 'RegExp') {
+		return new RegExp(obj);
+	} else if (checkType(obj) === 'Map') {
+		const newMap = new Map();
+		obj.forEach((value, key) => {
+			if (typeof value === 'object') {
+				newMap.set(key, deepClone(value));
+			} else {
+				newMap.set(key, value);
+			}
+		});
+		return newMap;
+	} else if (checkType(obj) === 'Set') {
+		const newSet = new Set();
+		obj.forEach((value) => {
+			if (typeof value === 'object') {
+				newSet.add(deepClone(value));
+			} else {
+				newSet.add(value);
+			}
+		});
+		return newSet;
+	} else {
+		return obj;
+	}
+}
+
+const a = {
+	name: 'aaa',
+	skills: ['踢球', '跑步', '打羽毛球'],
+	age: 18,
+	love: {
+		name: '小红',
+		age: 16,
+	},
+	map: new Map([['aaa', '123']]),
+	fn: function (a, b, c) {
+		console.log(`我的名字叫${this.name}` + a);
+		return b + c;
+	},
+	set: new Set([1, 2, 3, 4, 5]),
+	date: new Date(),
+	reg: /^[1,2]/,
+	symbol: Symbol('abc'),
+};
+// a.loop = a;
+const newA = deepClone(a);
+a.age = 100;
+a.love.age = 100;
+a.set.add('1123');
+a.skills.push('计算机');
+a.name = 'bbb';
+a.map.set('name', '小明');
+
+console.log(a);
+console.log(newA);
+
+// console.log(a.fn('a', 1, 2));
+// console.log(newA.fn('newA', 1, 3));
+// console.log(a.fn === newA.fn);
+// console.log(a.date === newA.date);
+// console.log(a.reg === newA.reg);
+// console.log(a.symbol === newA.symbol);
---
+++ javascript/handwrite/手写深拷贝.js
@@ -0,0 +1,96 @@
+const isDateType = (data) =>
+	(typeof data === 'function' || typeof data === 'object') && data !== null;
+
+/**
+ * 实现深度克隆（基本上能够满足深度克隆的所有要求）
+ * 1. 考虑各种数据类型，存在构造函数的可以用 new Xxxx()
+ * 2. 如果存在循环引用，使用 WeekMap 记录
+ * @param {*} obj
+ * @param {*} hash
+ */
+function deepClone(obj, hash = new WeakMap()) {
+	if (obj.constructor === Date) {
+		return new Date(obj); // 如果是日期对象，直接返回一个新的日期对象
+	} else if (obj.constructor === RegExp) {
+		return new RegExp(obj); // 如果是正则对象，直接放回一个新的正则对象
+	} else if (obj.constructor === Function) {
+		return new Function(`return ${obj.toString()}`).call(this);
+	} else if (obj.constructor === Map) {
+		const map = new Map();
+		obj.forEach((value, key) => {
+			if (typeof value === 'object') {
+				map.set(key, deepClone(value, hash));
+			} else {
+				map.set(key, value);
+			}
+		});
+		return map;
+	} else if (obj.constructor === Set) {
+		const set = new Set();
+		obj.forEach((value) => {
+			if (typeof value === 'object') {
+				set.add(deepClone(value, hash));
+			} else {
+				set.add(value);
+			}
+		});
+		return set;
+	}
+
+	// 如果循环引用了，就使用 WeekMap 解决
+	if (hash.has(obj)) {
+		return hash.get(obj);
+	}
+
+	// 遍历传入参数所有键的特性
+	let allDesc = Object.getOwnPropertyDescriptors(obj);
+
+	// 继承原型链
+	let cloneObj = Object.create(Object.getPrototypeOf(obj), allDesc);
+
+	hash.set(obj, cloneObj);
+	for (let key of Reflect.ownKeys(obj)) {
+		cloneObj[key] = isDateType(obj[key]) ? deepClone(obj[key], hash) : obj[key];
+	}
+
+	return cloneObj;
+}
+
+// 验证对象
+let obj = {
+	num: 0,
+	str: '',
+	boolean: true,
+	unf: undefined,
+	nul: null,
+	obj: { name: '我是一个对象', id: 1 },
+	arr: [0, 1, 2],
+	func: function () {
+		console.log('我是一个函数');
+	},
+	date: new Date(0),
+	reg: new RegExp('/我是一个正则/ig'),
+	[Symbol('1')]: 1,
+	map: new Map([
+		[1, 2],
+		[2, 3],
+		[3, 4],
+		[4, { a: 1, b: 2 }],
+	]),
+	set: new Set([1, 2, 3, 4, 5, { a: 1, b: 2 }]),
+};
+
+Object.defineProperty(obj, 'innumerable', { enumerable: 'false', value: '不可枚举属性' });
+obj = Object.create(obj, Object.getOwnPropertyDescriptors(obj));
+obj.loop = obj; // 设置 loop 成循环引用的属性
+let cloneObj = deepClone(obj);
+cloneObj.arr.push(4);
+// console.log(
+// 	obj,
+// 	cloneObj,
+// 	obj.func === cloneObj.func,
+// 	obj.map === cloneObj.map,
+// 	obj.set === cloneObj.set,
+// );
+// obj.func();
+// cloneObj.func();
