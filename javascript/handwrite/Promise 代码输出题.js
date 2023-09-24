afcdd322de19fc51533d9147c692491f53889fa4
qishilong <663253062@qq.com>
2023-09-24 10:03:59 +0800

    Promise 代码输出题
    Promise 代码输出题

---
+++ javascript/handwrite/Promise 代码输出题.js
@@ -0,0 +1,34 @@
+/**
+ * 1
+ */
+async function async1() {
+	console.log('async1 start');
+	await async2();
+	console.log('async1 end');
+}
+async function async2() {
+	console.log('async2');
+}
+console.log('script start');
+setTimeout(function () {
+	console.log('setTimeout');
+}, 0);
+async1();
+new Promise(function (resolve) {
+	console.log('promise1');
+	resolve();
+	console.log('promise2');
+}).then(function () {
+	console.log('promise3');
+});
+console.log('script end');
+
+// script start
+// async1 start
+// async2
+// promise1
+// promise2
+// script end
+// async1 end
+// promise3
+// setTimeout
