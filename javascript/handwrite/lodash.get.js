fd521400af862d61200d93b3b99a472d1eaf1978
qishilong <663253062@qq.com>
2023-09-24 21:39:57 +0800

    lodash.get
    lodash.get

--- javascript/handwrite/lodash.get.js
+++ javascript/handwrite/lodash.get.js
@@ -1,3 +1,11 @@
+/**
+ * lodash.get
+ * 1. 通过正则把[]访问形式改成 .
+ * 2. 针对.进行分割，然后对每一项进行依次的遍历，找最后一级的value
+ * @param {*} obj
+ * @param {*} path
+ * @param {*} defaultValue
+ */
 function get(obj, path, defaultValue = 'default') {
 	if (typeof path !== 'string') {
 		throw new TypeError('Expect path is a character string');
