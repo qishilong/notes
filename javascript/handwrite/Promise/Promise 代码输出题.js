ca1d82e23310a73a9a46e60dee8b43e8b669006a
qishilong <663253062@qq.com>
2024-01-10 15:38:47 +0800

    ci(javascript): handwrite Promise 代码输出题

--- javascript/handwrite/Promise/Promise 代码输出题.js
+++ javascript/handwrite/Promise/Promise 代码输出题.js
@@ -2,26 +2,26 @@
  * 1
  */
 async function async1() {
-	console.log('async1 start');
-	await async2();
-	console.log('async1 end');
+  console.log("async1 start");
+  await async2();
+  console.log("async1 end");
 }
 async function async2() {
-	console.log('async2');
+  await console.log("async2");
 }
-console.log('script start');
+console.log("script start");
 setTimeout(function () {
-	console.log('setTimeout');
+  console.log("setTimeout");
 }, 0);
 async1();
 new Promise(function (resolve) {
-	console.log('promise1');
-	resolve();
-	console.log('promise2');
+  console.log("promise1");
+  resolve();
+  console.log("promise2");
 }).then(function () {
-	console.log('promise3');
+  console.log("promise3");
 });
-console.log('script end');
+console.log("script end");
 
 // script start
 // async1 start
