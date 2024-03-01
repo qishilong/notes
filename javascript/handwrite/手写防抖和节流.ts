f11361a1ef81ba7dcd0bb02176b84a7604180b52
qishilong <663253062@qq.com>
2024-03-01 20:43:32 +0800

    ci(js): handwrite 手写防抖和节流

--- javascript/handwrite/手写防抖和节流.ts
+++ javascript/handwrite/手写防抖和节流.ts
@@ -1,16 +1,37 @@
-// 函数防抖
-function debounce(callback: Function, time: number) {
-  let timer: number | undefined = undefined;
-  return function () {
-    if (timer) {
-      clearTimeout(timer);
-    }
-    const args = arguments;
-    timer = setTimeout(function () {
-      callback.apply(null, args);
-    }, time);
+/**
+ * 函数防抖
+ * 写法1
+ * @param fn
+ * @param wait
+ */
+// function debounce(callback: Function, time: number) {
+//   let timer: number | undefined = undefined;
+//   return function () {
+//     if (timer) {
+//       clearTimeout(timer);
+//     }
+//     const args = arguments;
+//     timer = setTimeout(function () {
+//       callback.apply(null, args);
+//     }, time);
+//   };
+// }
+
+/**
+ * 函数防抖
+ * 写法二
+ * @param fn
+ * @param wait
+ */
+const debounce = <F extends (...arg: any[]) => void>(fn: F, wait: number) => {
+  let timeId: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<F>) => {
+    clearTimeout(timeId);
+    timeId = setTimeout(() => {
+      fn(...args);
+    }, wait);
   };
-}
+};
 
 // 函数节流
 function throttle(callback: Function, time: number, isImmediately: boolean) {
